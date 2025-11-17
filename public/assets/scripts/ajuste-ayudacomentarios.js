9038b619ed3813eaf05436b4f8fe4073d23d2df6
JavierChMr <chumpitazjavier44@gmail.com>
2025-11-16 22:48:23 -0500

    ayuda-comentarios

---
+++ public/assets/scripts/ajuste-ayudacomentarios.js
@@ -0,0 +1,11 @@
+function irCentroAyuda() {
+    console.log("Centro de Ayuda");
+}
+
+function irEnviarComentarios() {
+    console.log("Enviar comentarios");
+}
+
+function irInfoApp() {
+    console.log("Info de la aplicación");
+}
